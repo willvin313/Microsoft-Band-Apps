527117d389463ed9287de9fe894d0843e7f56b8c
KRK <kanchirk@hotmail.com>
2016-01-07 06:52:51 -0500

    Created Two Sample App Projects. Working on Helper Classes for simplified Reuse.

---
+++ Record It Now/Helper/BandHelper.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.Band;
+using System.Threading.Tasks;
+
+namespace Record_It_Now.Helper {
+  public class BandHelper {
+    static public IBandInfo[] PairedBands { get; set; }
+    static public IBandClient BandClient { get; set; }
+
+    public string FirmwareVersion { get; set; }
+    public string HardwareVersion { get; set; }
+
+
+    public async Task<bool> LoadPairedBands() {
+      PairedBands = await BandClientManager.Instance.GetBandsAsync();
+      return true;
+    }
+
+
+    public async Task<bool> GetBandClient() {
+      try {
+        using (IBandClient bandClient = await BandClientManager.Instance.ConnectAsync(PairedBands[0])) {
+
+        }
+      } catch {
+        return false;
+      }
+      return true;
+    }
+
+    public async void LoadBandVersion() {
+      try {
+        if (string.IsNullOrWhiteSpace(FirmwareVersion) || string.IsNullOrWhiteSpace(HardwareVersion)) {
+          using (IBandClient bandClient = await BandClientManager.Instance.ConnectAsync(PairedBands[0])) {
+            FirmwareVersion = await bandClient.GetFirmwareVersionAsync();
+            HardwareVersion = await bandClient.GetHardwareVersionAsync();
+          }
+        }
+        // do work with firmware & hardware versions
+      } catch (BandException ex) {
+        // handle any BandExceptions
+      }
+    }
+
+
+
+  }
+}
---
+++ Record It Now/Helper/BandTileHelper.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Band.Tiles;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Record_It_Now.Helper {
+  public class BandTileHelper {
+    public IEnumerable<BandTile> CurrentBandTiles { get; set; }
+    public async Task<IEnumerable<BandTile>> GetExistingTilesAsync() {
+      CurrentBandTiles = await BandHelper.BandClient.TileManager.GetTilesAsync();
+      return CurrentBandTiles;
+    }
+  }
+}
